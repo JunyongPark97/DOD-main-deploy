6782ca933d20b5db397315dbb482cf56a9aaeea3
ChanYeongShin <ubuntu@ip-172-31-2-50.ap-northeast-2.compute.internal>
2021-06-25 11:26:02 +0000

    [UPDATED] update backend & frontend code

--- DOD-main-server/dod/dod/settings.py
+++ DOD-main-server/dod/dod/settings.py
@@ -67,7 +67,8 @@ THIRD_APPS = [
     # 'wpadmin',
     'storages',
     'debug_toolbar',
-    'crispy_forms'
+    'crispy_forms',
+    'django_crontab'
 ]
 
 INSTALLED_APPS += SECONDS_APPS + THIRD_APPS
